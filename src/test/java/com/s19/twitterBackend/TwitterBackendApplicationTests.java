57dffc104b0373a610ac07e780cd3a0640ac612f
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2025-03-01 14:57:05 +0300

    Initial Commit

---
+++ src/test/java/com/s19/twitterBackend/TwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s19.twitterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
