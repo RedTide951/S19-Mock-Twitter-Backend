d3baf83cec19bc84251007f8f3aed7bf693de50d
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2025-03-01 16:01:49 +0300

    dependencies

--- src/main/java/com/s19/twitterBackend/model/User.java
+++ src/main/java/com/s19/twitterBackend/model/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -12,6 +14,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 
     private String username;
 
